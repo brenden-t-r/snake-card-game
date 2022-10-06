de0cc12855ca2baa3e97ccb10fca7b17d5e1bdf2
Brenden Romanowski <brenden.romanowski@gmail.com>
2022-10-06 16:27:20 -0500

    Adding card scriptable object

---
+++ Assets/Scripts/CardScriptableObject.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+
+public class CardScriptableObject : ScriptableObject
+{
+    public Sprite sprite;
+    public string title;
+    public string funFact;
+    public string description;
+    public Ecosystem ecosystem;
+    public List<Mechanic> mechanics;
+
+    public enum Ecosystem
+    {
+        DESERT=0,
+        FOREST=1,
+        JUNGLE=2,
+        WATER=3
+    }
+    
+    public enum Mechanic
+    {
+        VENOMOUS=0,
+        CANNIBAL=1
+    }
+
+    #if UNITY_EDITOR
+    [MenuItem("Assets/Create/Card")]
+    public static void CreateCard()
+    {
+        string path = EditorUtility.SaveFilePanelInProject(
+            "Save Card", "New Card", "Asset", "Save Card", "Assets");
+        if (path == "")
+            return;
+        AssetDatabase.CreateAsset(CreateInstance<CardScriptableObject>(), path);
+    }
+    #endif
+}
