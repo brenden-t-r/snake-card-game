315635be546de4c638842008c1b6678b03b9ed05
Brenden Romanowski <brenden.romanowski@gmail.com>
2022-10-07 18:15:46 -0500

    Draw cards to hand

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -1,18 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Hand : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject prefabCard;
+    private int numOfCards = 0;
+    private readonly float Z_OFFSET = 0.2f;
+    private readonly float LAYOUT_MIN_WIDTH = 0.2f;
+    private readonly float LAYOUT_PREFERRED_WIDTH = 2.8f;
+    private readonly int HAND_LAYER = 3;
+    
+    public void Draw()
     {
-        
+        numOfCards += 1;
+        GameObject card = Instantiate(prefabCard, transform);
+        card.layer = HAND_LAYER;
+        RectTransform rectTransform = card.GetComponent<RectTransform>();
+        LayoutElement layoutElement = card.AddComponent<LayoutElement>();
+        layoutElement.minWidth = LAYOUT_MIN_WIDTH;
+        layoutElement.preferredWidth = LAYOUT_PREFERRED_WIDTH;
+        rectTransform.position = new Vector3(0, 0, numOfCards * Z_OFFSET);
+        Refresh();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private readonly int PREFERRED_ORTHOGRAPHIC_SIZE = 4;
+    
+    void Refresh()
     {
-        
+        SetLayerAllChildren(transform, HAND_LAYER);
+    }
+    
+    void SetLayerAllChildren(Transform root, int layer)
+    {
+        var children = root.GetComponentsInChildren<Transform>(includeInactive: true);
+        foreach (var child in children)
+        {
+            child.gameObject.layer = layer;
+        }
     }
 }
