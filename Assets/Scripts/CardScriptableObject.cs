0e967d838b3d99ab1ec41e09fd9624ce67e22107
Brenden Romanowski <brenden.romanowski@gmail.com>
2022-12-30 10:34:47 -0600

    feature: Food cost

--- Assets/Scripts/CardScriptableObject.cs
+++ Assets/Scripts/CardScriptableObject.cs
@@ -1,33 +1,33 @@
-using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
 public class CardScriptableObject : ScriptableObject, CardBase
 {
-    public Material material;
+
     public Sprite sprite;
     public Sprite spriteInPlay;
     public string title;
     public int attack;
     public int health;
-    public string funFact;
-    public string description;
-    public Ecosystem ecosystem;
-    public List<Mechanic> mechanics;
-    
-    public enum Ecosystem
-    {
-        DESERT=0,
-        FOREST=1,
-        JUNGLE=2,
-        WATER=3
-    }
-    
-    public enum Mechanic
-    {
-        VENOMOUS=0,
-        CANNIBAL=1
-    }
+    public bool foodAmphibian;
+    public bool foodReptile;
+    public bool foodSmallMammal;
+    public bool foodLargeMammal;
+    public int foodRequirement;
+
+    // public enum Ecosystem
+    // {
+    //     DESERT=0,
+    //     FOREST=1,
+    //     JUNGLE=2,
+    //     WATER=3
+    // }
+    //
+    // public enum Mechanic
+    // {
+    //     VENOMOUS=0,
+    //     CANNIBAL=1
+    // }
     
     public CardBase.CardType GetCardType()
     {
