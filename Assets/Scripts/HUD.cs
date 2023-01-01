725faad917aa98f0ca9a5b4c00fc36d9d911cdbc
Brenden Romanowski <brenden.romanowski@gmail.com>
2022-12-31 18:12:58 -0600

    feature: Health bar, HUD

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    [SerializeField] private Slider healthMeter;
+    [SerializeField] private TMP_Text textHealth;
+    [SerializeField] private TMP_Text discardPileCount;
+    [SerializeField] private TMP_Text deckPileCount;
+    private int deckCount = 0;
+
+    private void Start()
+    {
+        Events.EventDrawCard.AddListener(_ => deckCount += 1); //Add
+        Events.EventPlayCardsChooseSlot.AddListener(_ => deckCount -= 1); //Remove
+    }
+
+    private void Update()
+    {
+        textHealth.text = "" + Math.Round(healthMeter.value * 100) + "/100";
+        deckPileCount.text = deckCount.ToString();
+    }
+}
