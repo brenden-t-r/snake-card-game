121fa9b463c00d5d925ad3279a562beab1859777
Brenden Romanowski <brenden.romanowski@gmail.com>
2023-01-11 19:39:22 -0600

    feature: Opponent play slots setup and rudimentary logic

--- Assets/Scripts/PlayBoard.cs
+++ Assets/Scripts/PlayBoard.cs
@@ -2,7 +2,8 @@
 
 public class PlayBoard : MonoBehaviour
 {
-    [SerializeField] private GameObject slots;
+    [SerializeField] private GameObject slotsPlayer;
+    [SerializeField] private GameObject slotsOpponent;
     [SerializeField] private GameObject cardInPlayPrefab;
     private CardScriptableObject cardToPlay;
     
@@ -10,21 +11,35 @@ public class PlayBoard : MonoBehaviour
     {
         Events.EventPlayCard.AddListener(PlayCardEvent);
         Events.EventPlayCardsChooseSlot.AddListener(PlayChooseSlotEvent);
+        OpponentEvents.EventPlayCard.AddListener(OpponentPlayCardEvent);
     }
 
     private void PlayCardEvent(CardScriptableObject cardScriptableObject)
     {
         cardToPlay = cardScriptableObject;
-        slots.SetActive(true);
+        slotsPlayer.SetActive(true);
     }
 
     private void PlayChooseSlotEvent(Vector3 slotPos)
     {
-        slots.SetActive(false);
+        slotsPlayer.SetActive(false);
+        DoPlayCard(cardToPlay, slotPos);
+        cardToPlay = null;
+    }
+
+    private void OpponentPlayCardEvent(CardScriptableObject cardScriptableObject, int slotIndex)
+    {
+        Vector3 pos = slotsOpponent
+            .GetComponentsInChildren<Transform>()[slotIndex+1] // 0 index is parent game object
+            .position;
+        DoPlayCard(cardScriptableObject, pos);
+    }
+
+    private void DoPlayCard(CardScriptableObject type, Vector3 slotPos)
+    {
         GameObject card = Instantiate(cardInPlayPrefab, slotPos, Quaternion.identity, transform);
         CardInPlay cardInPlay = card.GetComponent<CardInPlay>();
-        cardInPlay.SetType(cardToPlay);
-        cardInPlay.Initialize();
-        cardToPlay = null;
+        cardInPlay.SetType(type);
+        cardInPlay.Initialize();   
     }
 }
